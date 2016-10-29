96dc72ce8be58571a2cf2e2d8506035965f9eac7
Jah524 <SAISHIKI.Jah@gmail.com>
2016-10-29 12:15:30 +0900

    Add goo API

---
+++ text/src/goo.clj
@@ -0,0 +1,29 @@
+(ns goo
+  (:require [org.httpkit.client :as http]
+            [clojure.data.json :as json]))
+
+
+(defn text2morphs [text]
+  (let [result @(http/post "https://labs.goo.ne.jp/api/morph"
+                           {:keepalive 100000
+                            :timeout 4000
+                            :headers {"Content-Type" "application/json"}
+                            :body (json/write-str  {"app_id" "c260f221d40c764f71810cf53fc563e93636ab56ec2c607c52b0c9c416af2079" "sentence" text})})]
+    (-> result
+        :body
+        json/read-str
+        (get "word_list")
+        first)))
+
+
+(defn text2nouns [text]
+  (->> (text2morphs text)
+       (filter #(= (second %) "名詞"))
+       (map first)
+       (remove #(some (fn[x] (= x %)) ["何" "なに" "好き"]))
+       distinct))
+
+
+(text2nouns "隣の客はよく柿食う客だ")
+
+(text2morphs "隣の客はよく柿食う客だ")
--- text/src/text/handler.clj
+++ text/src/text/handler.clj
@@ -5,10 +5,13 @@
             [org.httpkit.server :refer [run-server]]
             [ring.middleware.reload :refer [wrap-reload]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [goo :refer [text2morphs text2nouns]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/text2morphs" {params :params} (apply str (interpose ", "(text2morphs (:text params)))))
+  (GET "/text2nouns" {params :params} (apply str (interpose ", "(text2nouns (:text params)))))
   (route/not-found "Not Found"))
 
 (def app
@@ -17,7 +20,6 @@
       handler/site
       (wrap-reload)
       ))
-
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 3003))]
