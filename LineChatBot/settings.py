4ad9d71215286ca9392bd9be8ce65648008e47ec
Kosuke Futamata <Matasuke@mbp2014mid.local>
2016-11-04 16:51:00 +0900

    initial

---
+++ LineChatBot/settings.py
@@ -0,0 +1,10 @@
+#This is setting file about line messeging api
+# The file contains channell id, channel secret and access token.
+# The file is connected with view.py file
+
+
+CHANNEL_ID = "  1485954194"
+CHANNEL_SECRET = "2193a3f8877ee68cdf91af23c52cc22c"
+ACCESS_TOKEN = "eyHeWJoz1EcOK+ybY0O/IdPbLtxqP/Rc5KkTQmsS8AgQ4HenRwBnHYE4mFSSbyLqibsgnKb9JS8AOOr26zU0qgKoH2Nn6NJpT7/6dBWrPVP0+TdAlbW6y4HOj2Mwc+ocmFwgTfhGatxFDc9KSBN19gdB04t89/1O/w1cDnyilFU="
+
+API_KEY="785964692e7965396f434f4c616959504378645573477838684b533170736c3844764c6b2e714531507931"
